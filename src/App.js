cc7ce68736f4a8b5c77a0f5fe25775c4f6bbd736
Isaac Ongera <isaacongera717@gmail.com>
2023-03-12 07:53:31 +0300

    added sign in

--- src/App.js
+++ src/App.js
@@ -1,204 +1,22 @@
- 
-
-import React, { Component } from "react";
-import Modal from "./components/Modal";
-import axios from 'axios';  
-
-
-//const tasks =[
-  //{
-    //id: 1,
-    //title: " music",
-    //description: "create an organized rafrobeat mx ",
-    //completed: true
-  //},
-  //{
-    //id:3,
-    //title: "weekly reports",
-    //description: "sending overdue reports",
-    //completed: false
-  //},
-  //{
-    //id: 2,
-    //title: "today reports",
-    //description: "fill outtoday report",
-    //completed: true
-  //}
-//]
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      modal: false,
-      viewCompleted: false,
-
-      activeItem: {
-        title: "",
-        description: "",
-        completed: false
-      },
-      taskList: []
-    };
-  }
-
-  // Add componentDidMount()
-  componentDidMount() {
-    this.refreshList();
-  }
-
- 
-  refreshList = () => {
-    axios   //Axios to send and receive HTTP requests
-      .get("http://127.0.0.1:9292/projects")
-      .then(res => this.setState({ taskList: res.data }))
-      .catch(err => console.log(err));
-  };
+import React from 'react';
+import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import MainRedister from './components/MainRedister';
+import Main from './Main';
 
 
-  displayCompleted = status => {
-    if (status) {
-      return this.setState({ viewCompleted: true });
-    }
-    return this.setState({ viewCompleted: false });
-  };
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={ <MainRedister/>}/>
+        <Route path='main' element={ <Main/>}/>
 
-
-  renderTabList = () => {
-    return (
-      <div className="my-5 tab-list">
-        <span
-          onClick={() => this.displayCompleted(true)}
-          className={this.state.viewCompleted ? "active" : ""}
-        >
-          completed
-            </span>
-        <span
-          onClick={() => this.displayCompleted(false)}
-          className={this.state.viewCompleted ? "" : "active"}
-        >
-          Incompleted
-            </span>
-      </div>
-    );
-  };
-
-  // Main variable to render items on the screen
-  renderItems = () => {
-    const { viewCompleted } = this.state;
-    const newItems = this.state.taskList.filter(
-      item => item.completed === viewCompleted
-    );
-    return newItems.map(item => (
-      <li
-        key={item.id}
-        className="list-group-item d-flex justify-content-between align-items-center"
-      >
-        <span
-          className={`todo-title mr-2 ${this.state.viewCompleted ? "completed-todo" : ""
-            }`}
-          title={item.description}
-        >
-          {item.title}
-        </span>
-        <span>
-          <button
-            onClick={() => this.editItem(item)}
-            className="btn btn-secondary mr-2"
-          >
-            Edit
-          </button>
-          <button
-            onClick={() => this.handleDelete(item)}
-            className="btn btn-danger"
-          >
-            Delete
-          </button>
-        </span>
-      </li>
-    ));
-  };
-  
-
-  ////add this after modal creation
-  toggle = () => {//add this after modal creation
-    this.setState({ modal: !this.state.modal });//add this after modal creation
-  };
- //  handleSubmit = item => {//add this after modal creation
-   //  this.toggle();//add this after modal creation
-    //alert("save" + JSON.stringify(item));//add this after modal creation
- //};
-
-  // Submit an item
-  handleSubmit = item => {
-    this.toggle();
-    if (item.id) {
-      // if old post to edit and submit
-      axios
-        .put(`http://127.0.0.1:9292projects/${item.id}/`, item)
-        .then(res => this.refreshList());
-      return;
-    }
-     //if new post to submit
-    axios
-      .post("http://127.0.0.1:9292/projects/", item)
-      .then(res => this.refreshList());
-  };
-
-  // Delete item
-  handleDelete = item => {
-    axios
-      .delete(`http://127.0.0.1:9292/projects/${item.id}/`)
-      .then(res => this.refreshList());
-};
- // handleDelete = item => {//add this after modal creation
-   // alert("delete" + JSON.stringify(item));//add this after modal creation
-  //};
-
-  // Create item
-  createItem = () => {
-    const item = { title: "", description: "", completed: false };
-    this.setState({ activeItem: item, modal: !this.state.modal });
-  };
-
-  //Edit item
-  editItem = item => {
-    this.setState({ activeItem: item, modal: !this.state.modal });
-  };
-
-
-  // -I- Start by visual effects to viewer
-  render() {
-    return (
-      <main className="content p-3 mb-2 bg-info">
-        <h1 className="text-white text-uppercase text-center my-4">Project Manager</h1>
-        <div className="row ">
-          <div className="col-md-6 col-sm-10 mx-auto p-0">
-            <div className="card p-3">
-              <div className="">
-                <button onClick={this.createItem} className="btn btn-primary">
-                  Add project
-                    </button>
-              </div>
-              {this.renderTabList()}
-              <ul className="list-group list-group-flush">
-                {this.renderItems()}
-              </ul>
-            </div>
-          </div>
-        </div>
-        <footer className="my-3 mb-2 bg-info text-white text-center">
-          copyright 2023 &copy; All Rights Reserved
-        </footer>
-        {this.state.modal ? (
-          <Modal
-            activeItem={this.state.activeItem}
-            toggle={this.toggle}
-            onSave={this.handleSubmit}
-          />
-        ) : null}
-      </main>
-    );
-  }
+      </Routes>
+      
+    </div>
+  );
 }
+
 export default App;
